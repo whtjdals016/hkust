4c23b5f4c776b7d26b6b70071a977afda82e3e32
Jimmy Kim <jkimab@ust.hk>
2015-02-09 22:33:00 +0800

    sell.php 조금 수정..
    재민이 register form 따서 수정 요망..

--- sell.php
+++ sell.php
@@ -6,11 +6,15 @@
 	<div class="content">
 		<div class="inner">
 			<div class="bodyframe">
-				<form action="sell_check.php" method="post" enctype="multipart/form-data">
-					Select image to upload:
-					<input type="file" name="fileToUpload" id="fileToUpload">
-					<input type="submit" value="Upload Image" name="submit">
-				</form>
+				<h4>Select files from your computer</h4>
+				  <form action="sell_check.php" method="post" enctype="multipart/form-data" id="js-upload-form">
+					<div class="form-inline">
+					  <div class="form-group">
+						<input type="file" name="fileToUpload" id="fileToUpload">
+					  </div>
+					  <button type="submit" class="btn btn-sm btn-primary" id="js-upload-submit">Upload files</button>
+					</div>
+				  </form>
 			</div>
 		</div>
 	</div>
